3da12d1639448874165b210f57a45a9e5e44981d
Jonah Graham <jonah@kichwacoders.com>
2015-09-04 13:45:40 +0100

    Remove non-dataset bundles

--- org.eclipse.dawnsci.nexus/src/org/eclipse/dawnsci/nexus/validation/NXApplicationValidator.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.dawnsci.nexus.validation;
-
-import org.eclipse.dawnsci.nexus.NXroot;
-
-public interface NXApplicationValidator {
-	
-	public void validate(final NXroot root) throws NexusValidationException, Exception;
-
-}
